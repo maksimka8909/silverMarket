85557f0eda632715898986af5fa803a7a5ff587b
Максим Кузнецов <kuznec8909@gmail.com>
2024-10-11 19:01:44 +0300

    feat: Добавление работы с БД. Запись добавления в корзину. Очистка из корзины. Добавление статических файлов

--- silverMarket/settings.py
+++ silverMarket/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,7 +120,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, media)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
@@ -128,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- silverMarket/views.py
+++ silverMarket/views.py
@@ -15,19 +15,19 @@ def index(request):
 def bucket(request):
     buckets = Bucket.objects.select_related('product')
     buckets_dict = {
-        'bucket': buckets
+        'buckets': buckets
     }
     return render(request, "bucketPage.html", buckets_dict)
-
-
-def contacts(request):
-    return render(request, "contactsPage.html")
 
 def addProduct(request, productId):
     product = Product.objects.get(id=productId)
     Bucket.objects.create(product=product)
-    products = Product.objects.all()
-    products_dict = {
-        'products': products
-    }
-    return render(request, "mainPage.html", products_dict)
+    return index(request)
+
+def clearBucket(request):
+    Bucket.objects.all().delete()
+    return bucket(request)
+
+def contacts(request):
+    return render(request, "contactsPage.html")
+
